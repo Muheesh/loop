56fb643a79bcc75fb6f4793a912c58a478c2733b
Muheesh <muheeshas@gmail.com>
2022-03-03 11:40:21 +0530

    elif

---
+++ elif.py
@@ -0,0 +1,7 @@
+a = int(input("Enter the number"))
+if a == 0:
+    print("Equal to 0")
+elif a >0 :
+    print("Greater than zero")
+else:
+    print("Less than zero")
---
+++ ifelse.py
@@ -0,0 +1,6 @@
+a = int(input("Enter the number to check"))
+
+if a<20:
+    print("It is less than 20")
+else:
+    print("It is greater than 20")
