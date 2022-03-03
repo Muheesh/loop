87295338bf2067b5e67361c13aac012061fc6c66
Muheesh <muheeshas@gmail.com>
2022-03-03 15:30:26 +0530

    sort

---
+++ list.py
@@ -0,0 +1,6 @@
+name = ["Muheesh","Sundar","Ashwin"]
+name.append("Sanjay")
+name.remove("Sundar")
+name[0] = "Kumar"
+name.sort()
+print(name)
---
+++ namesort.py
@@ -0,0 +1,7 @@
+name=[]
+for i in range(0,5):
+    name1 = input("Enter the name")
+    name.append(name1)
+
+name.sort()
+print(name)
